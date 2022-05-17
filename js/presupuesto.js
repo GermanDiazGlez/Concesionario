3d0030f8ff4c2f96bf62d4002f8080783252b323
Germán Díaz González <56638034+UO258472@users.noreply.github.com>
2022-05-17 14:39:10 +0200

    Vehiculos, base de datos, estadisticas, presupuestado, etc

---
+++ js/presupuesto.js
@@ -0,0 +1,9 @@
+var nombre = localStorage.getItem("nombre");
+var modelo = localStorage.getItem("modelo");
+var id = localStorage.getItem("id");
+
+$("#datosVehiculo1").html(nombre);
+$("#datosVehiculo2").html(modelo);
+
+$("input[name='marca']").val(nombre);
+$("input[name='id']").val(id);
